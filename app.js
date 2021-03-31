b216edfce382800ef1a9ee38ecd58b1e964918d7
bellkevin677 <bellkevin677@gmail.com>
2021-03-31 10:03:31 -0500

    complete

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const mulan = {
     title: `Mulan`,
     main: `Fa Mulan`,
 
-    quote: function(){
+    quote: function () {
         return `Dishonor! Dishonor on your whole family!`;
     },
-    storyline: function(){
-        return `In the movie ${this.title}, to save her father from death in the army, a young maiden named ${this.main} secretly goes in his place and becomes one of Chins'a greatest heroines in the process.`;
+    storyline: function () {
+        return `The movie ${this.title} is about ${this.main}.`;
     }
 }
 
@@ -14,35 +14,77 @@ const tangled = {
     title: 'Tangled',
     main: `Rapunzel`,
 
-    quote: function(){
+    quote: function () {
         return `I can't believe I did this! I can't believe I did this! I have to go home! I am never going back! I'm a horrible duaghter! BEST DAY EVER!`;
     },
 
-    storyline: function(){
-        return `In ${this.title}, The magically long-haired ${this.main} has spent her entire life in a tower, but now that a runaway thief has stumbled upon her, she is about to discover the world for the first time, and who she really is.`
+    storyline: function () {
+        return `The movie ${this.title} is about ${this.main}.`
     }
 }
 
-function DisneyMovie(title, main){
-    this.title = title
-    this.main = main
+function DisneyMovie(title, main) {
+    this.title = title;
+    this.main = main;
 }
 
-DisneyMovie.prototype.mulan.storyline = function(){
-    return `In the movie ${this.title}, to save her father from death in the army, a young maiden named ${this.main} secretly goes in his place and becomes one of Chins'a greatest heroines in the process.`;
-}
-
-DisneyMovie.prototype.tangled.storyline = function(){
-    return `In ${this.title}, The magically long-haired ${this.main} has spent her entire life in a tower, but now that a runaway thief has stumbled upon her, she is about to discover the world for the first time, and who she really is.`
+DisneyMovie.prototype.storyline = function () {
+    return `The movie ${this.title} is about ${this.main}.`;
 }
 
 const mulan1 = new DisneyMovie(`Mulan`, `Fa Mulan`);
 
 const tangled1 = new DisneyMovie(`Tangled`, `Rapunzel`);
 
-class DM{
-    constructor(title, main){
+class DM {
+    constructor(title, main) {
         this.title = title;
         this.main = main;
     }
+    storyline() {
+        return `The movie ${this.title} is about ${this.main}.`;
+    }
+    static loveDisneyMovies() {
+        return `I Love Disney Movies`;
+    }
 }
+
+const mulan2 = new DM(`Mulan`, `Fa Mulan`);
+
+const tangled2 = new DM(`Tangled`, `Rapunzel`);
+
+console.log(DM.loveDisneyMovies());
+
+class DMCast extends DM {
+    constructor(title, main, cast) {
+        super(title, main);
+        this.cast = cast;
+    }
+    static create(title, main, cast){
+        return new DMCast(title, main, cast);
+    }
+}
+
+const mulan3 = new DMCast(
+    `Mulan`,
+    `Fa Mulan`,
+    {
+        mulan: `Ming-Na Wen`,
+        mushu: `Eddie Murphy`,
+        shang: `BD Wong`,
+        theEmperor: `Pat Morita`
+    }
+);
+
+const repunzel3 = new DMCast(
+    `Tangled`,
+    `Rapunzel`,
+    {
+        rapunzel: `Mandy Moore`,
+        flynnRider: `Zachary Levi`,
+        motherGothel: `Donna Murphy`
+    }
+);
+
+
+
